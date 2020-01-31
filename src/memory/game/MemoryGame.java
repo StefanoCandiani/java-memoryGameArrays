c9e7bce4294a8d9f744c0480bf376b9349498f3e
22500 <22500@MXL52836SG.columbushs.com>
2020-01-31 14:33:20 -0500

    WIP

--- src/memory/game/MemoryGame.java
+++ src/memory/game/MemoryGame.java
@@ -1,21 +1,30 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package memory.game;
-
+import java.util.Random;
 /**
  *
  * @author mlarrubia
  */
 public class MemoryGame {
 
+    static Random r = new Random();
+    static final int GRIDSIZE = 4;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        char[] deck = {'A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H'};
+        boolean[] cardDrawn = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,};
+        char[][] gridArea = new char[GRIDSIZE][GRIDSIZE];
+        
+        for(int i = 0; i < deck.length; i++){
+            
+        }
     }
     
 }
